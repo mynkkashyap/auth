a9b0507347628b676bb4b96e2baf18468e724e37
mayank kashyap <78128668+mynkkashyap@users.noreply.github.com>
2026-01-10 00:44:38 +0530

    Update google.ts

--- functions/auth/google.ts
+++ functions/auth/google.ts
@@ -1,10 +1,14 @@
 export async function onRequest({ env }) {
+  const redirectUri = env.BASE_URL + "/auth/callback";
+
   const url =
     "https://accounts.google.com/o/oauth2/v2/auth" +
     "?client_id=" + env.GOOGLE_CLIENT_ID +
-    "&redirect_uri=" + env.BASE_URL + "/auth/callback" +
+    "&redirect_uri=" + encodeURIComponent(redirectUri) +
     "&response_type=code" +
-    "&scope=openid email profile";
+    "&scope=" + encodeURIComponent("openid email profile") +
+    "&access_type=online" +
+    "&prompt=select_account";
 
   return Response.redirect(url, 302);
 }
