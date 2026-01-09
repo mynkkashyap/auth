302bf5b89fa8d4dccd8c5ceee291e8d486b6796e
mayank kashyap <78128668+mynkkashyap@users.noreply.github.com>
2026-01-09 23:56:59 +0530

    Update google.ts

--- functions/auth/google.ts
+++ functions/auth/google.ts
@@ -1,9 +1,10 @@
 export async function onRequest({ env }) {
   const url =
     "https://accounts.google.com/o/oauth2/v2/auth" +
-    "?client_id=" + env.662047030240-ndto429qnhd8sc8rr3ennlbv17bqg2h7.apps.googleusercontent.com +
-    "&redirect_uri=" + env.auth.pages.dev + "/auth/callback" +
+    "?client_id=" + env.GOOGLE_CLIENT_ID +
+    "&redirect_uri=" + env.BASE_URL + "/auth/callback" +
     "&response_type=code" +
     "&scope=openid email profile";
+
   return Response.redirect(url, 302);
 }
