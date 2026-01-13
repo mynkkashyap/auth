c80f37d2872126673a426963f79cdcb574986f9f
mayank kashyap <78128668+mynkkashyap@users.noreply.github.com>
2026-01-13 17:36:46 +0530

    Create authors.ts

---
+++ functions/auth/authors.ts
@@ -0,0 +1,9 @@
+export async function onRequestGet({ env }) {
+  const { results } = await env.DB
+    .prepare("SELECT id, name, email FROM users ORDER BY id DESC")
+    .all();
+
+  return new Response(JSON.stringify(results), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
